87a240e4d35251725f63f57d500026f925385737
Leon <646027023@qq.com>
2019-05-18 22:45:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/game/game2048/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.game.game2048;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvScore;
+    private static MainActivity mainActivity = null;
+    private int score = 0;
+
+    public MainActivity(){
+        mainActivity = this;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvScore = (TextView) findViewById(R.id.tvScore);
+    }
+    public void clearScore(){
+        score = 0;
+        showScore();
+    }
+    public void showScore(){
+        tvScore.setText(score+"");
+    }
+    public void addScore(int s){
+        score += s;
+        showScore();
+    }
+    public static MainActivity getMainActivity() {
+        return mainActivity;
+    }
+    public int getScore() {
+        return score;
+    }
+}
